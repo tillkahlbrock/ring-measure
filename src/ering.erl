e20e4cf6691afea78874c9f145c41d2526de1243
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-11-15 15:22:48 +0100

    adds multiple measure roundtrips

--- src/ering.erl
+++ src/ering.erl
@@ -1,18 +1,24 @@
 -module(ering).
 
--export([start/1, measure/2, rpc/2]).
+-export([start/1, measure/2]).
 
 start(NumProcesses) ->
   MasterPid = spawn(fun() -> build_ring_master(NumProcesses, self()) end),
   MasterPid.
 
-measure(_Message, _Times) -> ok.
+measure(MasterPid, Roundtrips) -> measure(MasterPid, Roundtrips, []).
 
-rpc(MasterPid, Command) -> MasterPid ! {first, Command}.
+measure(_MasterPid, 0, Result) -> Result;
+
+measure(MasterPid, Roundtrips, Result) ->
+  NewResult = start_roundtrip(MasterPid),
+  measure(MasterPid, Roundtrips - 1, [NewResult|Result]).
+
+start_roundtrip(MasterPid) -> MasterPid ! {first, measure}.
 
 build_ring_master(NumProcesses, Master) ->
   Successor = spawn(fun() -> build_ring(NumProcesses - 1, Master) end),
-  receive_loop_master(Successor).
+  receive_loop_master(Successor, {0,0}).
 
 build_ring(NumProcesses, Master) when NumProcesses =< 1 ->
   receive_loop(Master);
@@ -21,16 +27,19 @@ build_ring(NumProcesses, Master) ->
   Successor = spawn(fun() -> build_ring(NumProcesses - 1, Master) end),
   receive_loop(Successor).
 
-receive_loop_master(Successor) ->
+receive_loop_master(Successor, Result = {StartTime, _EndTime}) ->
   receive
     kill -> Successor ! kill, io:format("I am dying... hardly....~n", []);
 	{first, Command} ->
+      NewStartTime = 1,
 	  Successor ! Command,
-      receive_loop_master(Successor);
+      receive_loop_master(Successor, {NewStartTime, 0});
     Command ->
+      EndTime = 5,
       io:format("Got: ~p~n", [Command]),
-      receive_loop_master(Successor)
-  end.
+      receive_loop_master(Successor, {StartTime, EndTime})
+  end,
+  Result.
 
 receive_loop(Successor) ->
   receive
